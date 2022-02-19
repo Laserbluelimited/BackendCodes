903a943e3406c4203f984cf5c9d3e34c7073a4c0
sazzeb sam <eko.5samuel@gmail.com>
2022-02-19 03:27:46 -0600

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn --bind 0.0.0.0:9194 skote.wsgi:application
+#uwsgi --socket :9194 --workers 4 --master --enable-threads --module skote.wsgi --static-map /static=/var/www/html/admin/assets/
