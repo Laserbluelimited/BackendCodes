b7c0ff4e8af2511b9b5a114ad5f72afc05ecbd18
Sarah Unokerieghan <sarahunoke@gmail.com>
2022-03-10 20:37:36 +0100

    resolving frontend

--- client_mgt/views.py
+++ client_mgt/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class InternetClientTableView(View):
+    pass
+
+class CorporateClientTableView(View):
+    pass
+
+class InternetClientRegistrationView(View):
+    pass
+
+class CorporateClientRegistrationView(View):
+    pass
