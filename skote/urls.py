60d8cbb685d0f55fdf40718df98f66951362ac5f
Sarah Unokerieghan <sarahunoke@gmail.com>
2022-03-23 08:35:09 +0100

    corrected header

--- skote/urls.py
+++ skote/urls.py
@@ -12,7 +12,15 @@ app_name='skote'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('auth/', include("authentication.urls")),
+    # Allauth
+    path('account/', include('allauth.urls')),
+    path('auth-logout/',TemplateView.as_view(template_name="account/logout-success.html"),name ='pages-logout'),
+    path('auth-lockscreen/',TemplateView.as_view(template_name="account/lock-screen.html"),name ='pages-lockscreen'),
+    #     #Custum change password done page redirect
+    # path('accounts/password/change/', login_required(MyPasswordChangeView.as_view()), name="account_change_password"),
+    # #Custum set password done page redirect
+    # path('accounts/password/set/', login_required(MyPasswordSetView.as_view()), name="account_set_password"),
     #display
     path('', include("display.urls")),
     # path('clinic', include("clinic_mgt.urls")),
